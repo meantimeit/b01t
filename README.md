486ee5b56d1cd3030dcbb177d40872a91849d582
Paul Connolley <paul.connolley@gmail.com>
2014-10-23 00:21:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# mPass Client Core
+
+[![browser support](https://ci.testling.com/$YOUR_USERNAME_HERE/max-by.png)
+](https://ci.testling.com/$YOUR_USERNAME_HERE/max-by)
+
+Core use cases for mPass
---
+++ test/entity_pair_creator_pgp/index-test.js
@@ -0,0 +1,20 @@
+var test = require('tape');
+var pgpPairCreator = require('lib/entity/pair_creator/pgp');
+
+test('Valid public key block', function (t) {
+    t.plan(2);
+    pgpPairCreator('jane <jane@example.org>', 'p455w0rd', function (err, key) {
+        t.ok(/-----BEGIN PGP PUBLIC KEY BLOCK-----/.test(key.public));
+        t.ok(/-----END PGP PUBLIC KEY BLOCK-----/.test(key.public));
+        t.end();
+    });
+})
+
+test('Valid private key block', function (t) {
+    t.plan(2);
+    pgpPairCreator('jane <jane@example.org>', 'p455w0rd', function (err, key) {
+        t.ok(/-----BEGIN PGP PRIVATE KEY BLOCK-----/.test(key.private));
+        t.ok(/-----END PGP PRIVATE KEY BLOCK-----/.test(key.private));
+        t.end();
+    });
+})
