c6bd8f9a26781e42c2744a757cc0c357c514d92b
Paul Connolley <paul.connolley@gmail.com>
2014-10-27 23:49:03 +0000

    Forgot to complete encryptable interface in PGP

--- test/encryptable-pgp/index-test.js
+++ test/encryptable-pgp/index-test.js
@@ -1,13 +1,16 @@
-//var test = require('tape');
-//var fs = require('fs');
-//var pgpMessageEncrypter = require('lib/encryptable/pgp');
+var test = require('tape');
+var fs = require('fs');
+var encryptablePgp = require('lib/encryptable/pgp');
+var decryptablePgp = require('lib/decryptable/pgp');
 
-//test('Encrypt HELLO', function (t) {
-//t.plan(1);
-//var expected = fs.readFileSync(__dirname + '/../test-data/encrypted/hello').toString();
-//var key = fs.readFileSync(__dirname + '/../test-data/keys/public_1.key').toString();
-//pgpMessageEncrypter(key, 'HELLO', function (err, message) {
-//fs.writeFileSync('/Users/connrs/Desktop/message', message);
-//t.equal(message, expected);
-//});
-//});
+test('Encrypt HELLO', function (t) {
+  t.plan(1);
+  var expected = fs.readFileSync(__dirname + '/../test-data/encrypted/hello').toString();
+  var publicKey = fs.readFileSync(__dirname + '/../test-data/keys/public_1.key').toString();
+  var privateKey = fs.readFileSync(__dirname + '/../test-data/keys/private_1.key').toString();
+  encryptablePgp(publicKey, 'HELLO', function (err, message) {
+    decryptablePgp(privateKey, 'p4ss', message, function (err, decryptedText) {
+      t.equal('HELLO', decryptedText);
+    });
+  });
+});
