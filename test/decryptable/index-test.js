b9d33305536a2ca5bcc09a4483eb1a08557958a2
Paul Connolley <paul.connolley@gmail.com>
2014-10-27 23:56:25 +0000

    Initial commit
    Extracted interfaces from the initial use cases in
    github.com/meantimeit/mpass-client-core

---
+++ test/decryptable/index-test.js
@@ -0,0 +1,21 @@
+var test = require('tape');
+var fs = require('fs');
+var decryptable = require('lib/decryptable');
+
+test('Decrypt HELLO', function (t) {
+  t.plan(1);
+  var encryptedMessage = fs.readFileSync(__dirname + '/../test-data/encrypted/hello').toString();
+  var key = fs.readFileSync(__dirname + '/../test-data/keys/private_1.key').toString();
+  decryptable(key, 'p4ss', encryptedMessage, function (err, message) {
+    t.equal(message, 'HELLO');
+  });
+});
+
+test('Invalid private key throws decrypt error', function (t) {
+  t.plan(1);
+  var encryptedMessage = fs.readFileSync(__dirname + '/../test-data/encrypted/hello').toString();
+  var key = fs.readFileSync(__dirname + '/../test-data/keys/invalid_private.key').toString();
+  decryptable(key, 'p4ss', encryptedMessage, function (err, message) {
+    t.ok(err instanceof Error);
+  });
+});
